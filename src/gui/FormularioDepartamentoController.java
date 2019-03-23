50ee7b6629238a3e1471d7e9770922f26314a49e
MatheusPedro <matheus.pedro1214@gmail.com>
2019-03-23 02:22:20 -0500

    Validação de dados e ValidationException

--- src/gui/FormularioDepartamentoController.java
+++ src/gui/FormularioDepartamentoController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -13,11 +15,12 @@ import gui.util.Utilidades;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Departamento;
+import model.exceptions.ValidationException;
 import model.services.DepartamentoServices;
 
 public class FormularioDepartamentoController implements Initializable{
@@ -69,6 +72,9 @@ public class FormularioDepartamentoController implements Initializable{
 			notifyDataChangeListeners();
 			Utilidades.stageAtual(event).close();
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
 		catch (DbException e) {
 			Alerts.mostrarAlerta("Erro ao salvar objeto", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -83,8 +89,19 @@ public class FormularioDepartamentoController implements Initializable{
 	private Departamento getDadosFormulario() {
 		Departamento obj = new Departamento();
 		
+		ValidationException exception = new ValidationException("Erro de validação");
+		
 		obj.setId(Utilidades.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "O campo não pode ser vazio");
+		}
+		
 		obj.setName(txtName.getText());
+		
+		if(exception.getErros().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -110,5 +127,14 @@ public class FormularioDepartamentoController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorNome.setText(errors.get("name"));
+		}
+		
 	}
 }
