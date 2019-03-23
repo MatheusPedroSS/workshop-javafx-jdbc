ca9f276a80cc2b3526706da2a00ca58f38bcbceb
MatheusPedro <matheus.pedro1214@gmail.com>
2019-03-23 00:30:46 -0500

    Formulario de Departamento

--- src/gui/DepartamentoListController.java
+++ src/gui/DepartamentoListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utilidades;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Departamento;
 import model.services.DepartamentoServices;
@@ -36,8 +45,9 @@ public class DepartamentoListController implements Initializable{
 	private ObservableList<Departamento> obsList;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Botão");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parenteStage = Utilidades.stageAtual(event);
+		criarDialogFormulario("/gui/FormularioDepartamento.fxml", parenteStage);
 	}
 	
 	public void setDepartamentoService(DepartamentoServices service) {
@@ -66,5 +76,24 @@ public class DepartamentoListController implements Initializable{
 		List<Departamento> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
+	}
+	
+	private void criarDialogFormulario(String absoluteName, Stage parenteStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com os dados do Departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parenteStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e) {
+			Alerts.mostrarAlerta("IO Exception", "Erro carregamento view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
